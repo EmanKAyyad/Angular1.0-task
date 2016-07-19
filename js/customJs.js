1712d5f837eb73299c4f6a6cb28baf41fa9e5566
eman_kh <eman_kh>
2016-07-19 16:49:36 +0300

    finally made it with animating insert new record divs, added buttons to the edit/new html file, installed the localstorage in angular and inserted key, values in it but in order to make multiple records with same key but different values i guess i would have to go with php and mysql so I would like to focus first on the homepage table and filter.

--- js/customJs.js
+++ js/customJs.js
@@ -1,4 +1,13 @@
-var myapp = angular.module('MyApp', ['ngRoute','ngMaterial', 'ngMessages']);
+var myapp = angular.module('MyApp', ['ngAnimate', 'ngRoute', 'ngMaterial', 'ngMessages', 'LocalStorageModule']);
+
+
+myapp.config(function (localStorageServiceProvider) {
+    localStorageServiceProvider
+            .setPrefix('ArticalStore')
+            .setStorageType('localStorage')
+            .setNotify(true, true);
+});
+
 
 myapp.config(function ($routeProvider) {
     $routeProvider
@@ -13,7 +22,12 @@ myapp.config(function ($routeProvider) {
             });
 
 });
-var flag = false;
+
+// getdate
+
+myapp.service('getDate',function(){
+    this.date = '';
+});
 
 myapp.controller('headerController', ['$scope', '$route', '$log', function ($scope, $route, $log) {
         $scope.refreshPage = function () {
@@ -22,13 +36,47 @@ myapp.controller('headerController', ['$scope', '$route', '$log', function ($sco
         };
     }]);
 
-myapp.controller('NewEditController', ['$scope', function ($scope) {
-        
-        $scope.showPartition = function(){
-           flag = true;
-           return flag;
+myapp.controller('NewEditController', ['$scope','localStorageService','getDate', function ($scope,localStorageService,getDate) {
+
+        $scope.lamp = false;
+        $scope.mydate =getDate.date;
+        $scope.toggle = function ($event) {
+//            console.log($event.target.parentNode);
+            var allpartitions = document.getElementsByClassName('insert-partition');
+            var partition = $event.target.parentNode;
+
+            if ($scope.lamp) {
+                $scope.lamp = false;
+                partition.style.height = "80px";
+                partition.style.overflow = "hidden";
+                partition.style.width = "45%";
+
+            }
+            else {
+                $scope.lamp = true;
+                for (var i = 0; i < allpartitions.length; i++) {
+                    allpartitions[i].style.height = "80px";
+                    allpartitions[i].style.overflow = "hidden";
+                    allpartitions[i].style.width = "45%";
+
+                    partition.style.height = "420px";
+                    partition.style.width = "50%";
+                }
+            }
+
+        };
+
+        $scope.newEditRecord = function () {
+            localStorageService.set('name', $scope.name);
+            localStorageService.set('author', $scope.author);
+            localStorageService.set('category', $scope.category);
+            localStorageService.set('state', $scope.state);
+            localStorageService.set('date', $scope.mydate);
+            console.log('done');
         };
     }]);
+
+
 myapp.controller('homeController', ['$scope', function ($scope) {
 
     }]);
@@ -36,7 +84,7 @@ myapp.controller('homeController', ['$scope', function ($scope) {
 
 //date picker code
 
-myapp.controller('AppCtrl', ['$scope', function ($scope) {
+myapp.controller('AppCtrl', ['$scope','getDate', function ($scope,getDate) {
         $scope.myDate = new Date();
         $scope.minDate = new Date(
                 $scope.myDate.getFullYear(),
@@ -50,4 +98,8 @@ myapp.controller('AppCtrl', ['$scope', function ($scope) {
             var day = date.getDay();
             return day === 0 || day === 6;
         };
+        $scope.$watch('myDate',function(){
+            getDate.date = $scope.myDate;
+        });
+        
     }]);
